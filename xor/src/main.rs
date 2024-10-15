1303aca77bab38704c1e396af54156e6b0dba19b
Paweł Rybak <87527837+wedkarz02@users.noreply.github.com>
2024-10-15 20:52:36 +0200

    feat: batman

--- xor/src/main.rs
+++ xor/src/main.rs
@@ -26,15 +26,33 @@ fn prepare_text() -> Result<(), Box<dyn Error>> {
     let text = fs::read_to_string("orig.txt")
         .map_err(|_| "Error: File 'orig.txt' not found.")?
         .replace("\n", " ");
+    let text = text_cleanup(text);
     let max_len = 64;
-    let mut plain_file = File::create("plain.txt")?;
+    let mut ctr = 0;
 
+    for _ in text.as_bytes().chunks(max_len) {
+        ctr += 1;
+    }
+
+    if ctr < 2 {
+        return Err("Error: Plaintext is too short for the cryptanalysis.".into());
+    }
+
+    let mut plain_file = File::create("plain.txt")?;
     for chunk in text.as_bytes().chunks(max_len) {
         let line = String::from_utf8_lossy(chunk);
         writeln!(plain_file, "{:width$}", line, width = max_len)?;
     }
 
     Ok(())
+}
+
+fn text_cleanup(input: String) -> String {
+    input
+        .chars()
+        .filter(|c| c.is_ascii_alphabetic() || *c == ' ')
+        .map(|c| c.to_ascii_lowercase())
+        .collect()
 }
 
 fn read_lines<P>(file_name: P) -> io::Result<Vec<String>>
@@ -46,6 +64,24 @@ where
     reader.lines().collect()
 }
 
+fn read_chunks<P>(file_name: P, chunk_size: usize) -> io::Result<Vec<String>>
+where
+    P: AsRef<Path>,
+{
+    let text = fs::read_to_string(file_name)?;
+    let mut chunks = Vec::new();
+
+    for chunk in text.as_bytes().chunks(chunk_size) {
+        let line = String::from_utf8_lossy(chunk).to_string();
+        if line.len() != 64 {
+            continue;
+        }
+        chunks.push(line);
+    }
+
+    Ok(chunks)
+}
+
 fn stream(text: &str, key: &str) -> String {
     text.chars()
         .zip(key.chars())
@@ -55,6 +91,7 @@ fn stream(text: &str, key: &str) -> String {
 
 fn encrypt() -> Result<(), Box<dyn Error>> {
     let plain_lines = read_lines("plain.txt").map_err(|_| "Error: File 'plain.txt' not found.")?;
+    println!("{:#?}", plain_lines);
     let key = fs::read_to_string("key.txt")
         .map_err(|_| "Error: File 'key.txt' not found.")?
         .trim()
@@ -96,7 +133,11 @@ fn main() -> Result<(), Box<dyn Error>> {
                 println!("{}", e);
             }
         }
-        Mode::Cryptanalysis => {}
+        Mode::Cryptanalysis => {
+            // if let Err(e) = cryptanalysis() {
+            //     println!("{}", e);
+            // }
+        }
     }
 
     Ok(())
